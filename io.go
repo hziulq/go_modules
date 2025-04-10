775c4fdbcee7bec2af0fe40c412453aacd3b96cc
hziulq <kd1295154@st.kobedenshi.ac.jp>
2025-04-11 07:26:49 +0900

    first commit

---
+++ io.go
@@ -0,0 +1,30 @@
+package io
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func Prompt(recommend string) (string, error) {
+	fmt.Print(recommend)
+	return InputString()
+}
+
+func InputString() (string, error) {
+	reader := bufio.NewReader(os.Stdin)
+
+	text, err := reader.ReadString('\n')
+
+	if err != nil {
+		return "", errors.New("ERROR InputString")
+	}
+
+	text = strings.TrimSuffix(text, "\n")
+	text = strings.TrimSuffix(text, "\r")
+
+	return text, nil
+
+}
